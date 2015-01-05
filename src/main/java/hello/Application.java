c0c9a62a7aed5065a0ca83a6c5ef31fb90b6f909
rusdlsla <rusdlsla@gmail.com>
2015-01-05 17:17:37 +0900

    build 확인 완료

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,6 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.Filter;
+import java.nio.charset.Charset;
 
 /**
  * Created by Administrator on 2014-12-29.
@@ -11,7 +18,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-//    extends SpringBootServletInitializer {
+//    extends SpringBootServletInitializer implements WebApplicationInitializer {
 
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
@@ -33,6 +40,21 @@ public class Application {
 //            }
 //        };
 //    }
+
+    // 결과를 출력시 강제로 UTF-8로 설정
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+    }
+
+    // POST 요청시 한글이 깨지는 문제를 보완
+    @Bean
+    public Filter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
